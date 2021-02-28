f3cdaa183e632aec13a0b74a601e1b18426d7148
Daniel Halligan <dan.halligan@fiosgenomics.com>
2021-02-28 21:08:22 +0000

    Add kmp solution

--- kmp/kmp.py
+++ kmp/kmp.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 
 from Bio import SeqIO
-from Bio.SeqUtils import GC
-import numpy as np
 import sys
 
-file = sys.argv[1]
-file = "test.txt"
-seq = list(SeqIO.parse(file, "fasta"))[0].seq
+def kmp(seq):
+    j = -1
+    b = [j]
+    for i in range(len(seq)):
+        while j >= 0 and seq[i] != seq[j]:
+            j = b[j]
+        j += 1
+        b.append(j)
+    return b[1:]
 
-p = [0]
-k = 1
-count = 0
-while k < len(seq):
-    if seq[k] == seq[count]:
-        count += 1
-        p.append(count)
-        k += 1
-    else:
-        if count > 0:
-            count = 0
-        else:
-            p.append(0)
-            k += 1
+seq = list(SeqIO.parse(sys.argv[1], "fasta"))[0].seq
+print(*kmp(seq))
